0bf7eba8c3f055d83cb9876f71cf79bb04d43c24
GODBINI <arashi0121@naver.com>
2019-08-26 19:00:07 +0900

    구글에서 제공하는 것만 입력

--- app/src/main/java/com/example/haniumqr/MainActivity.java
+++ app/src/main/java/com/example/haniumqr/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.haniumqr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.common.SignInButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SignInButton button = (SignInButton) findViewById(R.id.login_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("클릭 테스트");
+            }
+
+        });
     }
 }
